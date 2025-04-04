d5d52d51d1ea83d9ec590ca14cbc2b2e01aa295b
RajTechnosoft <erajmishra000@gmail.com>
2025-04-04 16:00:52 +0530

    the removing cart fixed

--- pages/user/MyCart.js
+++ pages/user/MyCart.js
@@ -1,9 +1,13 @@
-console.log("these responses are from mycart file ");
+// accessing the data from the localstorage of cart
+console.log("hii");
+
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
-// console.log(cart);
+//selecting the cart container to show the all cards in this container
 cartContainer = document.querySelector(".my-card-container");
+// function for display the carts in the cartContainer
 const displayCartProduct = function (cart) {
   cartContainer.innerHTML = cart
+    // for accessing all the data from the cart
     .map((item) => {
       return `<div class="card cart-product-card col-12 col-md-5 col-lg-3 p-0">
           <img
@@ -72,8 +76,8 @@ const displayCartProduct = function (cart) {
             </p>
           </div>
           <div class="btns row d-flex justify-content-evenly py-2 gap-2 gap-md-0 p-2">
-          <button class="RemoveBtn  col-12 col-md-6">
-           <i class="fa-solid fa-trash mx-2"></i></i>Remove Item
+         <button class="RemoveBtn col-12 col-md-6">
+            <i class="fa-solid fa-trash mx-2"></i>Remove Item
           </button>
            <button class="OrderBtn bg-success col-12  col-md-5">
             <i class="fa-solid fa-cart-plus mx-2"></i>Order Now
@@ -83,25 +87,40 @@ const displayCartProduct = function (cart) {
           
         </div>`;
     })
+    // to remove the commas
     .join("");
 };
-
+// show the data after all the content loaded
 document.addEventListener("DOMContentLoaded", function () {
+  // calling the function to show the carts in the product container here cart contains all the data from local storage
   displayCartProduct(cart);
+  // onclick function to remove the cart from my cart
+  // applying click event on the whole document
   document.addEventListener("click", (event) => {
+    // checking which button is remove button
     if (event.target.classList.contains("RemoveBtn")) {
+      // selcecting the exact cart which on the user clicked
       let ProductCart = event.target.closest(".cart-product-card");
-      // console.log(ProductCart);
+      // storing the title name to search related data of the cart from the cart array
       let ProductTitle = ProductCart.querySelector(".Product-title");
-      console.log(ProductTitle.innerText);
-      let updatedCart = cart.filter((item) => {
-        return ProductTitle.innerText != item.name;
+      // filtering the the data which have no this title and that's a way to remove clicked product
+      cart = cart.filter((item) => {
+        return item.name != ProductTitle.innerText;
       });
-      console.log("this is updated cart : ");
-      console.log(updatedCart);
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-      displayCartProduct(updatedCart);
+      // debugging starts --- completed successfully-- data was updated after the refresh because of fetck keyword
+      setTimeout(() => {
+        // console.log("this is data returned by filter function");
+        // console.log(cart);
+        // console.log("this is previous data");
+        // console.log(cart);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        // console.log("this is updated data");
+        // console.log(cart);
+        displayCartProduct(cart);
+      }, 10);
+      // debugging ends
     }
   });
 });
+
 // alert("hello");
