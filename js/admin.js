1e9c561e57957bc615a39f3019961bb877ca65f5
RajTechnosoft <erajmishra000@gmail.com>
2025-04-13 22:55:00 +0530

    the costomers page added

--- js/admin.js
+++ js/admin.js
@@ -44,3 +44,45 @@ function displayProductsData(container, data) {
     })
     .join("");
 }
+fetch("../../data/costomers.json")
+  .then((response) => {
+    // console.log("Response URL:", response.url);
+    if (!response.ok) throw new Error("HTTP error " + response.status);
+    return response.json();
+  })
+  .then((data) => {
+    // console.log("Loaded Data:", data);
+
+    let costomersDataContainer = document.getElementById(
+      "costomers-data-container"
+    );
+    // console.log(productDataContainer);
+    displayCostomersData(costomersDataContainer, data);
+  })
+  .catch((error) => console.error("Error loading JSON:", error));
+
+function displayCostomersData(container, data) {
+  console.log(container, data);
+  container.innerHTML = data
+    .map((item) => {
+      return `
+        <tr>
+                  <th scope="row">${item.id}</th>
+                  <td>${item.name}</td>
+                  <td>${item.email}</td>
+                  <td>+91 ${item.mobile}</td>
+                   <td>+91 ${item.registration_date}</td>
+                  <td>
+                    <img
+                      src="${item.profile_picture}"
+                      class="card-img-top w-25 h-25 img-fluid rounded-circle mx-auto"
+                      alt="..."
+                    />
+                  </td>
+                  <td><button class="btn btn-sm btn-danger">block</button></td>
+                  <td><button class="btn btn-sm btn-success">Announce</button></td>
+                </tr>
+        `;
+    })
+    .join("");
+}
